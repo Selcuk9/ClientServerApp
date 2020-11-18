4f45b0a1867f04c8e0606e45d417c7d8542e88bd
Selcuk Ilyazov <ilyazov49@gmail.com>
2020-11-18 11:11:13 +0300

    Добавьте файлы проекта.

---
+++ Client/Client.cpp
@@ -0,0 +1,36 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+#pragma warning(disable:4996)
+int main()
+{
+	WSAData wsaData;
+	WORD DllVersion = MAKEWORD(2, 1);
+
+	if (WSAStartup(DllVersion, &wsaData) != 0)
+	{
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeOfaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET connection = socket(AF_INET,SOCK_STREAM,NULL);
+	if (connect(connection,(SOCKADDR*)&addr,sizeof(addr)) != 0)
+	{
+		std::cout << "Error: failed connect to server.\n";
+		return 1;
+	}
+	std::cout << "Connected\n";
+	char msg[256];
+	system("pause");
+	recv(connection,msg,sizeof(msg),NULL);
+	std::cout << msg << std::endl;
+
+	return 0;
+}
---
+++ ServerS/ServerS.cpp
@@ -0,0 +1,49 @@
+﻿#pragma comment(lib,"Ws2_32.lib")
+
+#include <winsock2.h>
+#include <iostream>
+
+
+#pragma warning(disable:4996)
+int main(int argc, char* argv[])
+{
+	WSAData wsaData;
+	WORD DllVersion = MAKEWORD(2,1);
+	
+	if (WSAStartup(DllVersion, &wsaData) != 0)
+	{
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeOfaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+ 
+
+	SOCKET sListen = socket(AF_INET,SOCK_STREAM,NULL);
+	bind(sListen,(SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeOfaddr);
+
+	if (newConnection == 0)
+	{
+		std::cout << "Error #2\n";
+	}
+	else {
+		std::cout << "Client Connected!\n";
+		char msg[256] = "Hello. it's my first network program!";
+		send(newConnection,msg,sizeof(addr),NULL);
+	}
+
+	system("pause");
+
+	return 0;
+
+}
+
+
